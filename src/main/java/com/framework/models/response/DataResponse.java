ce808fa407f3b4e0afc8c1e0aa1fedd913428031
vikum sugathadasa <vikum1407@gmail.com>
2026-01-18 22:09:39 +0100

    Created pojo data and response class for POST response

--- src/main/java/com/framework/models/response/DataResponse.java
+++ src/main/java/com/framework/models/response/DataResponse.java
@@ -1,4 +1,16 @@
 package com.framework.models.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
 public class DataResponse {
+    private int year;
+    private double price;
+
+    @JsonProperty("CPU Model")
+    private String cpuModel;
+
+    @JsonProperty("Hard disk size")
+    private String hardDiskSize;
 }
--- src/main/java/com/framework/models/response/ObjectResponse.java
+++ src/main/java/com/framework/models/response/ObjectResponse.java
@@ -1,4 +1,11 @@
 package com.framework.models.response;
 
+import lombok.Data;
+
+@Data
 public class ObjectResponse {
+    private String id;
+    private String name;
+    private String createAt;
+    private DataResponse data;
 }
