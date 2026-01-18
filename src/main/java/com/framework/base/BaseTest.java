7a8c2e3d78a481f4deef3ab09b10950b1cf84d4d
vikum sugathadasa <vikum1407@gmail.com>
2026-01-18 21:11:13 +0100

    Created basetest file

---
+++ src/main/java/com/framework/base/BaseTest.java
@@ -0,0 +1,15 @@
+package com.framework.base;
+
+import com.framework.config.ConfigReader;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = ConfigReader.getProperty("base.url");
+    }
+
+
+}
