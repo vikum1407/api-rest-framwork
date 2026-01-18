6cc8df4df23df3bdbbf6c0d8ea84d5db73714bd8
vikum sugathadasa <vikum1407@gmail.com>
2026-01-18 21:59:33 +0100

    Created pojo data and request class for POST request

--- src/main/java/com/framework/models/request/Data.java
+++ src/main/java/com/framework/models/request/Data.java
@@ -1,4 +1,19 @@
 package com.framework.models.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@lombok.Data
 public class Data {
+    private int year;
+    private double price;
+
+    /**
+     * @JsonProperty is mandatory for space-containing fields.
+     */
+    @JsonProperty("CPU Model")
+    private String cpuModel;
+
+    @JsonProperty("Hard disk size")
+    private String hardDiskSize;
+
 }
--- src/main/java/com/framework/models/request/ObjectRequest.java
+++ src/main/java/com/framework/models/request/ObjectRequest.java
@@ -1,4 +1,9 @@
 package com.framework.models.request;
 
+import lombok.Data;
+
+@Data
 public class ObjectRequest {
+    private String name;
+    private com.framework.models.request.Data data;
 }
