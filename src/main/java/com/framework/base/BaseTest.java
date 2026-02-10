18e263333c2e1f9d6709e6d0cada8beeef000cb9
vikum sugathadasa <vikum1407@gmail.com>
2026-02-10 14:36:29 +0100

    Added request and response specification to load url and check beforeClass

--- src/main/java/com/framework/base/BaseTest.java
+++ src/main/java/com/framework/base/BaseTest.java
@@ -2,13 +2,28 @@ package com.framework.base;
 
 import com.framework.config.ConfigReader;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 
 public class BaseTest {
+    RequestSpecification reqSpec;
+    ResponseSpecification resSpec;
 
     @BeforeClass
     public void setup() {
-        RestAssured.baseURI = ConfigReader.getProperty("base.url");
+        reqSpec = new RequestSpecBuilder()
+                .setBaseUri(ConfigReader.getProperty("base.url"))
+                .setContentType(ContentType.JSON)
+                .build();
+
+        resSpec = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(200)
+                .build();
     }
 
 }
